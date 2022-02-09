8e3c14e37d934767bdba6baf5a12de63be3e473e
Isaac Friedman <isaac@isaac-friedman.com>
2022-02-09 12:13:28 -0500

    list function

--- app/javascript/components/Home.jsx
+++ app/javascript/components/Home.jsx
@@ -1,32 +1,32 @@
-import React, {useState, useEffect} from 'react';
+import React, { useState, useEffect } from 'react';
 import Loader from './Loader';
 import Pending from './Pending';
 import Completed from './Completed';
 
-const Home = () => {
-	const [todos, setTodos] = useState({});
-	const [loading, setLoading] = useState(true);
 
-	useEffect(() => {
-		const url = "todos/all"
-		fetch(url)
-			.then(response => {
-				if (response.ok) {
-					return response.json();
-				} else {
-					throw new Error("Network response was not OK.");
-				}
-			})
-			.then(response => {
-				setTodos(response);
-				setLoading(false);
-			})
-			.catch(() => console.log("An Error occurred:"));
-		}, []);
+const Home  = () => {
+  const [todos, setTodos] = useState({});
+  const [loading, setLoading] = useState(true);
 
-		return (
-			<div className="vw-100 vh-100 primary-color d-flex justify-content-center">
-				<div className="jumbotron jumbotron-fluid bg-transparent">
+  useEffect(() => {
+    const url = "/todos/all";
+    fetch(url)
+      .then(response => {
+        if (response.ok) {
+          return response.json();
+        }
+        throw new Error("Network response was not ok.");
+      })
+      .then(response => {
+        setTodos(response);
+        setLoading(false);
+      })
+      .catch(() => console.log('An error occurred while fetching the todos'));
+  }, []);
+
+  return (
+    <div className="vw-100 vh-100 primary-color d-flex justify-content-center">
+      <div className="jumbotron jumbotron-fluid bg-transparent">
         <div className="container secondary-color">
           <h1 className="display-4">Todo</h1>
           <p className="lead">
@@ -36,16 +36,16 @@ const Home = () => {
           {
             loading ? <Loader /> : (
               <div>
-                <Pending pending={todos.pending} />
+                <Pending pending={todos.incomplete} />
                 <hr className="my-4" />
                 <Completed completed={todos.completed} />
               </div>
             )
           }
         </div>
-    	  </div>
-			</div>
-		)
-	}
+      </div>
+    </div>
+  )
+}
 
 export default Home;
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,4 @@
 Rails.application.routes.draw do
-  get 	'todos/index'
-	root 	'todos#index'
-	get 	'todos/all'
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  root "todos#index"
+  get "todos/all"
 end
