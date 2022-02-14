0467c877780c36d645e88cf33175beb21cee4aea
Isaac Friedman <isaac@isaac-friedman.com>
2022-02-14 10:39:30 -0500

    update controller

--- app/controllers/todos_controller.rb
+++ app/controllers/todos_controller.rb
@@ -7,4 +7,16 @@ class TodosController < ApplicationController
 		incomplete = Todo.where(completed: false)
 		render json: {completed: completed, incomplete: incomplete}
 	end
+
+	def update
+		to_update = Todo.find(params[:id])
+    if to_update.update_attributes!(todo_params)
+      render json: {message: "Item updated successfully"}
+    else
+      render json: {message: "Operation failed successfully"}
+    end
+  end
+
+  private todo_params
+    params.require(:todo).permit(:id, :title, :completed)
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
   root "todos#index"
   get "todos/all"
+	put "todos/update"
 end
