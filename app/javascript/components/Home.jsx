d54b6ce5ac34e066a26d595400f8e6ab07c33c62
Isaac Friedman <isaac@isaac-friedman.com>
2022-02-15 11:41:57 -0500

    create task

--- app/javascript/components/Home.jsx
+++ app/javascript/components/Home.jsx
@@ -7,6 +7,8 @@ import Completed from './Completed';
 const Home  = () => {
   const [todos, setTodos] = useState({});
   const [loading, setLoading] = useState(true);
+  const [addTodo, setAddTodo] = useState(false);
+  const [todo, setTodo] = useState('')
 
   useEffect(() => {
     const url = "/todos/all";
@@ -24,15 +26,78 @@ const Home  = () => {
       .catch(() => console.log('An error occurred while fetching the todos'));
   }, []);
 
+  const showAddTodo = () => {
+    setAddTodo(true);
+  }
+
+  const cancelAdd = () => {
+    setAddTodo(false);
+  }
+
+  const handleChange = () => {
+    setTodo(event.target.value)
+  }
+
+  const handleSubmit = () => {
+    event.preventDefault();
+
+    if (todo === '') {
+      return true;
+    }
+
+    const todoBody = {
+      title: todo,
+      completed: false
+    };
+
+    const url = 'todos/create';
+    const token = document.querySelector('meta[name="csrf-token"]').content;
+
+    fetch(url, {
+      method: "POST",
+      headers: {
+        "X-CSRF-TOKEN": token,
+        "Content-type": "application/json"
+      },
+      body: JSON.stringify(todoBody)
+    })
+      .then(response => {
+        if(response.ok) {
+          return response.json();
+        }
+        throw new Error("Network error creating new todo");
+      })
+      .then(response => {
+        console.log(response);
+        window.location.reload(false);
+      })
+      .catch(() => console.log('An error occurred while adding the todo item'));
+  }
   return (
     <div className="vw-100 vh-100 primary-color d-flex justify-content-center">
       <div className="jumbotron jumbotron-fluid bg-transparent">
         <div className="container secondary-color">
           <h1 className="display-4">Todo</h1>
           <p className="lead">
-            A curated list of recipes for the best homemade meal and delicacies.
+            Keep track of your tasks and assorted nonsense. IDK the todo app is the hello world of web frameworks.
           </p>
           <hr className="my-4" />
+         {
+           !addTodo && <button type="button" className="btn btn-primary align-right" onClick={showAddTodo}>Add Todo</button>
+         }
+         {
+           addTodo && (
+             <form className="add-todo" onSubmit={handleSubmit}>
+               <div className="form-row">
+                 <div className="col-9">
+                   <input type="text" className="form-control-plaintext mr-3" autoFocus placeholder="Todo Item" onChange={handleChange} />
+                 </div>
+                 <button type="submit" className="btn btn-primary col mr-2">Add</button>
+                 <button className="btn btn-outline-primary col" onClick={cancelAdd}>Cancel</button>
+               </div>
+             </form>
+           )
+         }
           {
             loading ? <Loader /> : (
               <div>
